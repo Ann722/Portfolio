f1ce9f8092e5defb0766478744aa5e6be3ed5b15
Ann722 <anagui0783@gmail.com>
2024-03-14 13:58:21 +0100

    On y croit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://ann722.github.io/Portfolio/",
+  base: '/Portfolio',
+});
